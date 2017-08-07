fba584ab1d6b0d8049fcee39a7a260a5a76783d9
mustachesolutions <enriquenery00@hotmail.com>
2017-08-06 18:52:59 -0600

    Initial commit
    Amazing Movie Trailers webpage

---
+++ README.md
@@ -0,0 +1,38 @@
+# AMAZING MOVIE TRAILERS
+
+This is the source code for a Movie Trailer webpage. It displays a list of movie trailers picked personally by Milton Ramirez. You can see a little description of the movie when you just hover over the movie image. 
+
+# STEPS: 
+1. Clone this repository into your desired location
+2. Open IDLE (Python GUI)
+3. Go to File -> Open and load the file: "entertaintment_center.py"
+4. Simply run the entertaintment_center.py and It will generate the HTML file with the movie trailers and it will be open automatically.
+5. Have fun! 
+
+# FILES DECRIPTION
+1. **media . py** -> Contains the `Movie` class definition.
+2. **movie_trailers.py** -> creates the HTML file for display the movie trailers in the browser.
+3. **entertaintment_center.py** -> Creates instances of the Movie Class and calls the movie_trailers.py to generate the web page. 
+
+# COPYRIGT
+##### MIT License
+Copyright (c) 2017 Milton Enrique Ramírez Nery
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+* Attribution back to its creator.
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
---
+++ entertaintment_center.py
@@ -0,0 +1,45 @@
+import movie_trailers
+import media
+
+#Instances creation
+
+interstellar = media.Movie("Interstellar",
+                            "A team of explorers travel through a wormhole in space in an attempt to ensure humanity's survival.",
+                            "https://images-na.ssl-images-amazon.com/images/M/MV5BMjIxNTU4MzY4MF5BMl5BanBnXkFtZTgwMzM4ODI3MjE@._V1_SY1000_CR0,0,640,1000_AL_.jpg",
+                            "https://www.youtube.com/watch?v=zSWdZVtXT7E")
+
+guardians_of_the_galaxy = media.Movie("Guardians of the Galaxy 2",
+                            "The Guardians must fight to keep their newfound family together as they unravel the mystery of Peter Quill's true parentage.",
+                            "https://images-na.ssl-images-amazon.com/images/M/MV5BMTg2MzI1MTg3OF5BMl5BanBnXkFtZTgwNTU3NDA2MTI@._V1_SY1000_CR0,0,674,1000_AL_.jpg",
+                            "https://www.youtube.com/watch?v=duGqrYw4usE")
+
+iron_man_three = media.Movie("Iron Man 3",
+                            "When Tony Stark's world is torn apart by a formidable terrorist called the Mandarin, he starts an odyssey of rebuilding and retribution.",
+                            "https://images-na.ssl-images-amazon.com/images/M/MV5BMTkzMjEzMjY1M15BMl5BanBnXkFtZTcwNTMxOTYyOQ@@._V1_SY1000_SX700_AL_.jpg",
+                            "https://www.youtube.com/watch?v=Ke1Y3P9D0Bc")
+
+thor_ragnarok = media.Movie("Thor Ragnarok",
+                            "Imprisoned, the mighty Thor finds himself in a lethal gladiatorial contest against the Hulk, his former ally. Thor must fight for survival and race against time to prevent the all-powerful Hela from destroying his home and the Asgardian civilization.",
+                            "https://images-na.ssl-images-amazon.com/images/M/MV5BMjMyNDkzMzI1OF5BMl5BanBnXkFtZTgwODcxODg5MjI@._V1_SY1000_CR0,0,674,1000_AL_.jpg",
+                            "https://www.youtube.com/watch?v=ue80QwXMRHg")
+
+capitain_america = media.Movie("Capitain America: Civil War",
+                            "Political interference in the Avengers' activities causes a rift between former allies Captain America and Iron Man.",
+                            "https://images-na.ssl-images-amazon.com/images/M/MV5BMjQ0MTgyNjAxMV5BMl5BanBnXkFtZTgwNjUzMDkyODE@._V1_SY1000_CR0,0,674,1000_AL_.jpg",
+                            "https://www.youtube.com/watch?v=dKrVegVI0Us")
+
+deadpool = media.Movie("Deadpool",
+                            "A fast-talking mercenary with a morbid sense of humor is subjected to a rogue experiment that leaves him with accelerated healing powers and a quest for revenge.",
+                            "https://images-na.ssl-images-amazon.com/images/M/MV5BMjQyODg5Njc4N15BMl5BanBnXkFtZTgwMzExMjE3NzE@._V1_SY1000_SX686_AL_.jpg",
+                            "https://www.youtube.com/watch?v=0JnRdfiUMa8")
+
+#Generating array of instances created                                   
+movies = [interstellar,
+          guardians_of_the_galaxy, 
+          iron_man_three, 
+          thor_ragnarok, 
+          capitain_america, 
+          deadpool]
+
+#Creating web page
+movie_trailers.open_movies_page(movies)
