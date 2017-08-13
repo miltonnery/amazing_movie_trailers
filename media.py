a1347d1bf9c388485c456f8484231b57d587a36f
Milton Enrique Ramírez Nery <enriquenery00@hotmail.com>
2017-08-13 16:12:42 -0600

    Code style corrections.

--- media.py
+++ media.py
@@ -1,15 +1,16 @@
+"""Imports"""
 import webbrowser
 
 
 class Movie():
-    
-    #Movie class definition
-	def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
-		self.title = movie_title
-		self.storyline = movie_storyline
-		self.poster_image_url = poster_image
-		self.trailer_youtube_url = trailer_youtube
-        
-    #Opens a trailer url in the browser    
-	def show_trailer(self):
-		webbrowser.open(self.trailer_youtube_url)
+    """Movie class definition."""
+    def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):  # NOQA
+        """Instantiates the Movie Class."""
+        self.title = movie_title
+        self.storyline = movie_storyline
+        self.poster_image_url = poster_image
+        self.trailer_youtube_url = trailer_youtube
+
+    def show_trailer(self):
+        """Opens a trailer url in the browser."""
+        webbrowser.open(self.trailer_youtube_url)
